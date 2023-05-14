3793a93fb39913e28ecba31ed197a7f3c971b5c8
Lucas Zacarias <63613740+Lucas-Zacarias@users.noreply.github.com>
2023-05-14 12:25:26 -0300

    Update ApiInterface.kt

--- app/src/main/java/com/example/apistest/ApiInterface.kt
+++ app/src/main/java/com/example/apistest/ApiInterface.kt
@@ -9,7 +9,7 @@ import retrofit2.http.POST
 interface ApiInterface {
 
     companion object{
-        const val API_KEY = "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"
+        const val API_KEY = "no colocar api"
     }
 
     @POST("completions")
